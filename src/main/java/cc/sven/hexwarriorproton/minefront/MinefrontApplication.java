5187317d09f9a356c6447b50204b05086edc501a
Sven Carrillo Castillo <carrillo.castillo.sven@googlemail.com>
2023-01-01 21:15:45 +0100

    init

---
+++ src/main/java/cc/sven/hexwarriorproton/minefront/MinefrontApplication.java
@@ -0,0 +1,13 @@
+package cc.sven.hexwarriorproton.minefront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinefrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinefrontApplication.class, args);
+	}
+
+}
