3099261e0ef0df97da962795c9c950acb4dacd8d
Sven Carrillo Castillo <carrillo.castillo.sven@googlemail.com>
2023-01-08 04:34:37 +0100

    fixed single&multithreaded handling. both is possible now.

--- src/main/java/cc/sven/hexwarriorproton/minefront/engine/graphics/renderer/RendererTemplate.java
+++ src/main/java/cc/sven/hexwarriorproton/minefront/engine/graphics/renderer/RendererTemplate.java
@@ -26,13 +26,64 @@ abstract class RendererTemplate implements Renderable {
     public RendererTemplate(@NonNull final RendererProperties rendererProperties, @NonNull final ARGBCalculatorProvider argbCalculatorProvider) {
         this.rendererProperties = rendererProperties;
         this.argbCalculatorProvider = argbCalculatorProvider;
-
         if (rendererProperties.isParallelizeRendering()) {
             executorService = Executors.newFixedThreadPool(rendererProperties.getThreadPoolSize());
         }
     }
 
     public void render(@NonNull final Scanable source, @NonNull final Bufferable target, final int xOffset, final int yOffset) {
+        if (executorService == null) {
+            renderSinglethreaded(source, target, xOffset, yOffset);
+        } else {
+            renderMultithreaded(source, target, xOffset, yOffset);
+        }
+    }
+
+    @Override
+    public void renderMergeable(@NonNull final Mergeable source, @NonNull final PixelBuffer targetBuffer, final int xOffset, final int yOffset) {
+        source.mergeBufferWith(targetBuffer, xOffset, yOffset);
+    }
+
+    @Override
+    public void renderMergeable(@NonNull final Mergeable source, @NonNull final Bufferable target, final int xOffset, final int yOffset) {
+        source.mergeBufferWith(target, xOffset, yOffset);
+    }
+
+    @Override
+    public void setPixelAt(@NonNull final Bufferable target, final int x, final int y, final int newPixelValue) {
+        if (x < 0 || y < 0) return;
+        if (x > target.getDimension().getWidthX() || y > target.getDimension().getHeightY()) return;
+
+        target.bufferPixelAt(x, y, newPixelValue);
+    }
+
+    private void renderSinglethreaded(@NonNull final Scanable source, @NonNull final Bufferable target, final int xOffset, final int yOffset) {
+        for (int y = 0; y < source.getDimension().getHeightY(); y++) {
+            final int copyToY = y + yOffset;
+            if (copyToY < 0 || copyToY >= target.getDimension().getHeightY()) {
+                continue;
+            }
+
+            final int yFinal = y;
+            for (int x = 0; x < source.getDimension().getWidthX(); x++) {
+                final int copyToX = x + xOffset;
+                if (copyToX < 0 || copyToX >= target.getDimension().getWidthX()) continue;
+
+                final int colorValue = source.scanPixelAt(x, yFinal);
+                if (colorValue == 0xFFff00ff) continue;
+                final int alphaComponent = argbCalculatorProvider.provide().getAlphaComponent(colorValue);
+                if (alphaComponent < 0xFF) {
+                    // set color blending (alpha channel)
+                    target.bufferPixelAt(copyToX, copyToY, argbCalculatorProvider.provide().blend(colorValue, target.scanPixelAt(copyToX, copyToY)));
+                } else {
+                    // set color without blending
+                    target.bufferPixelAt(copyToX, copyToY, colorValue);
+                }
+            }
+        }
+    }
+
+    private void renderMultithreaded(@NonNull final Scanable source, @NonNull final Bufferable target, final int xOffset, final int yOffset) {
         final CountDownLatch latch = new CountDownLatch(source.getDimension().getHeightY());
         for (int y = 0; y < source.getDimension().getHeightY(); y++) {
             final int copyToY = y + yOffset;
@@ -71,24 +122,6 @@ abstract class RendererTemplate implements Renderable {
             e.printStackTrace();
 //            System.exit(999);
         }
-    }
-
-    @Override
-    public void renderMergeable(@NonNull final Mergeable source, @NonNull final PixelBuffer targetBuffer, final int xOffset, final int yOffset) {
-        source.mergeBufferWith(targetBuffer, xOffset, yOffset);
-    }
-
-    @Override
-    public void renderMergeable(@NonNull final Mergeable source, @NonNull final Bufferable target, final int xOffset, final int yOffset) {
-        source.mergeBufferWith(target, xOffset, yOffset);
-    }
-
-    @Override
-    public void setPixelAt(@NonNull final Bufferable target, final int x, final int y, final int newPixelValue) {
-        if (x < 0 || y < 0) return;
-        if (x > target.getDimension().getWidthX() || y > target.getDimension().getHeightY()) return;
-
-        target.bufferPixelAt(x, y, newPixelValue);
     }
 
 }
